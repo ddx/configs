c8c7dfc6ef3831b3f288acd0db59e4622cc04eb2
Deven Xie <devenx@gmail.com>
2016-01-19 13:36:49 -0500

    add network interface stats message

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,13 @@
 apt-get update
 apt-get install -y build-essential
 
+# Some post-provisioning stats
+echo -e "\n\n\n======== PROVISION COMPLETE ========"
+
+echo -e "\nProvision runtime: $SECONDS seconds\n\n"
+
+echo -e "Guest VM Network Interfaces"
+echo -e "--------------------------------"
+ip -f inet -o addr | awk 'gsub("/.*$", "")'
+
 echo "Provisioning Complete!"
